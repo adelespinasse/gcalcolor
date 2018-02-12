f09faf923f02a3ae53bdc48be2b7220a59dd3b08
Alan deLespinasse <aldel@alum.mit.edu>
2018-02-12 13:38:45 -0500

    Fixes #1 and #2

--- content.js
+++ content.js
@@ -1,8 +1,13 @@
+// Content script for the extension. Does all the work.
+
 "use strict";
 
 // Colorizes an event element. Finds the colored dot, then sets the
 // overall color to that dot's color (which is its borderColor; the
-// dot is just an empty 0x0 element with a circular border).
+// dot is just an empty 0x0 element with a circular border). Also
+// hides the dot, since it is no longer needed to show the color, and
+// reduces padding to help line up events and let you see more of
+// their names.
 function colorizeEvent(eventEl) {
   let dotEl = eventEl;
   for (let i=0; i<3; i++) {
@@ -12,8 +17,10 @@ function colorizeEvent(eventEl) {
   }
   let color = dotEl.style.borderColor;
   if (!color)
-    return;
+    return; // Probably not a timed event
   eventEl.firstChild.style.color = color;
+  eventEl.firstChild.style.padding = '0';
+  dotEl.style.display = 'none';
 }
 
 // Colorizes all visible events.
@@ -24,20 +31,41 @@ function colorizeAll() {
   }
 }
 
-// Colorizes all visible events multiple times, after different
-// amounts of delay. This is because we don't have a good way to
-// intercept Google's redraw events. We have to just intercept user
-// input events that might trigger UI updates, and then update a few
-// times after that.
-function colorizeAfterThis(event) {
-  for (let delay of [1, 250, 1000, 3000, 10000])
-    setTimeout(colorizeAll, delay);
+
+// We don't have a precise way to know when Google Calendar has drawn
+// some new events on the screen. Instead we use a MutationObserver to
+// watch for DOM changes anywhere on the page. It would be really
+// inefficient to run colorizeAll every time we got an observer
+// callback, so instead we wait for a short delay to see if any more
+// callbacks happen. If so, we reset the timer and wait again. We call
+// colorizeAll only when the timer completes without another callback.
+//
+// Because there are a lot of irregularly timed screen updates when
+// the page is first being loaded, we set the delay to half a second
+// at first. After five seconds, we set it to 50 milliseconds for a
+// faster response to small updates.
+
+let timeoutId = null;
+let observerDelay = 500;
+setTimeout(() => { observerDelay = 50; }, 5000);
+
+function postObserverCallbacks() {
+  timeoutId = null;
+  colorizeAll();
 }
 
-// Colorize after loading the page.
-colorizeAfterThis();
+function observerCallback(mutationList) {
+  if (timeoutId)
+    clearTimeout(timeoutId);
+  timeoutId = setTimeout(postObserverCallbacks, observerDelay);
+}
 
-// Colorize whenever the user does anything that might trigger an update.
-document.body.addEventListener('mouseup', colorizeAfterThis, true);
-document.body.addEventListener('keyup', colorizeAfterThis, true);
-document.body.addEventListener('wheel', colorizeAfterThis, true);
+let observer = new MutationObserver(observerCallback);
+observer.observe(
+  document.body,
+  {
+    childList: true,
+    attributes: true,
+    subtree: true
+  }
+);
